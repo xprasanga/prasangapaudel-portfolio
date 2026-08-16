a3b52c2588f2fe13c8afcb5355a445a29668561d
Prasanga <paudelxprasanga@gmail.com>
2026-08-15 23:46:42 -0500

    Add lightweight portfolio JavaScript

---
+++ js/main.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',()=>{document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const id=a.getAttribute('href');const target=document.querySelector(id);if(target){e.preventDefault();target.scrollIntoView({behavior:'smooth',block:'start'})}}))});
